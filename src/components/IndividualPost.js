17cfb55c48a730fce111fd93bd5e54756a918d9e
sajanshrestha <sajanstha201.55@gmail.com>
2024-01-18 00:00:11 +0530

    Updates

--- src/components/IndividualPost.js
+++
@@ -1,9 +0,0 @@
-import './IndividualPost.css';
-function IndividualPost({post}){
-    return(
-        <div className='individual-post-container'>
-            <img src={post.src} className='post-layout'></img>
-        </div>
-    );
-}
-export default IndividualPost;
